cde735bed1520a9180d9b4c1fc11032fc0f183af
Bob <wastchenko.constantin@gmail.com>
2024-09-13 13:47:50 -0400

    redirect to og client

---
+++ ui/src/app/components/onboarding/Redirect.tsx
@@ -0,0 +1,16 @@
+export const Redirect = () => {
+  return (
+    <div className="flex flex-col justify-center items-center text-xl">
+      <p>This client's features have been merged within the original client!</p>
+      <p>Play your heart out: </p>
+      <a
+        href="https://lootsurvivor.io/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-gray-400"
+      >
+        https://lootsurvivor.io/
+      </a>
+    </div>
+  );
+};
